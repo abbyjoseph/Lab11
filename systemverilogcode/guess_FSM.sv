2695b1b3c9e826d7021362b974781079182d5162
abbyjoseph <abby_joseph1@baylor.edu>
2020-11-19 11:47:40 -0600

    start lab11

---
+++ systemverilogcode/guess_FSM.sv
@@ -0,0 +1,32 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/19/2020 11:46:18 AM
+// Design Name: 
+// Module Name: guess_FSM
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module guess_FSM(
+    input [4:0] b,
+    input rst,
+    input clk,
+    output win,
+    output lose,
+    output [4:0] y
+   
+    );
+endmodule
